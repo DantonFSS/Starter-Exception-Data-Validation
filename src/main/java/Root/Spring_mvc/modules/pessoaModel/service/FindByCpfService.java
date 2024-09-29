48c252ff1011f496777406a9ec7a3ac0f16913df
DantonFSS <dantonfilipe.s@gmail.com>
2024-09-28 22:11:16 -0300

    raw:add FindByIdService class file

---
+++ src/main/java/Root/Spring_mvc/modules/pessoaModel/service/FindByCpfService.java
@@ -0,0 +1,19 @@
+package Root.Spring_mvc.modules.pessoaModel.service;
+
+import Root.Spring_mvc.modules.pessoaModel.infra.database.repository.PessoaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FindByCpfService {
+
+
+    private final PessoaRepository pRepo;
+
+    public String getUserByCpf(String cpf) {
+        pRepo.findByCpf(cpf);
+        return cpf;
+    }
+
+}
