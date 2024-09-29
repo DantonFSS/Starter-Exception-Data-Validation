ee0aba7b90ad4edab5ecd4cc4ab51352994da194
DantonFSS <dantonfilipe.s@gmail.com>
2024-09-28 22:18:25 -0300

    raw:add specific error constants class file

---
+++ src/main/java/Root/Spring_mvc/shared/common/utils/consts/UserErrorConsts.java
@@ -0,0 +1,6 @@
+package Root.Spring_mvc.shared.common.utils.consts;
+
+public class UserErrorConsts {
+    public static final String CPF_DOES_NOT_MATCH = "Cpf não está no formato - XXX.XXX.XXX-XX...";
+    public static final String CPF_CANNOT_BE_NULL = "Cpf cannot be null";
+}
